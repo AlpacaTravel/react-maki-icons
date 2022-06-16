4ee73c8822534ac18e1aba7b980c20455272af87
Cameron Manderson <cam@alpaca.travel>
2022-06-17 08:01:21 +1000

    Updates example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 
-// import { Airport } from "@alpaca-travel/react-maki-icons/index-11.js";
-// import { Airport } from "@alpaca-travel/react-maki-icons/index-15.js";
-// import Commercial, {
-//   paths
-// } from "@alpaca-travel/react-maki-icons/icons/commercial-15";
-// console.log(paths);
-
-import { Commercial15, iconsMap } from "@alpaca-travel/react-maki-icons";
-console.log(iconsMap);
+import { Commercial, iconsMap } from "@alpaca-travel/react-maki-icons";
 
 function App() {
   return (
     <div>
-      <Commercial15 />
+      <Commercial />
     </div>
   );
 }
